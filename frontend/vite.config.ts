2fe1efbe2e19d1ff7b96f1c6847f8cabb04fdaaf
solethus <solethu@ssongca.com>
2024-10-19 21:20:46 +0200

    frontend: update Github page base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   // Base path to deploy to GitHub Pages
-  base: "/example-meeting-notes/",
+  base: "/meeting-notes/",
   plugins: [react()],
 });
